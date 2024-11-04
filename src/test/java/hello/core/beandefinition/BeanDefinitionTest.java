dfd22dbfc106dfcb3f70103d136dc2a1014d7477
yessh <keh1058@gmail.com>
2024-11-05 00:01:40 +0900

    싱글톤(스프링 컨테이너, @Configuration)

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -12,6 +12,11 @@ public class BeanDefinitionTest {
 //    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig .class);
     GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
     @Test
     @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean(){
